1af507312efb8e1ec76d9ca9a9ef7bf37bbcde34
mariahelenanoren <marialinder97@hotmail.com>
2020-11-23 11:18:20 +0100

    imported weather api and valid city list for api

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-window.onload = main;
+window.addEventListener("load", main);
 
 function main() {
-    
+    getApi();
+    getCityList();
+    setEventListeners();
+}
+
+function setEventListeners() {
+}
+
+async function getApi() {
+    country = getCountryInput();
+    try {
+        const result = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + country + "&appid=b7971cfa6d28e17a0ec0694039f7dfaf")
+        const data = await result.json();
+        console.log(data)
+    } catch(error) {
+    }
+}
+
+async function getCityList() {
+    try {
+        const result = await fetch("./current.city.list.json")
+        const data = await result.json();
+        console.log(data)
+    } catch(error) {
+    }
+}
+
+function getCountryInput() {
+    return "Ockelbo";
 }
