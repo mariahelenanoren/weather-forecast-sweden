ba5e8b3d2c1aed7583116a50befec024d87e7140
mariahelenanoren <marialinder97@hotmail.com>
2020-11-24 12:25:52 +0100

    broke apart js code and have managed to get data for latest hour

---
+++ weather.js
@@ -0,0 +1,20 @@
+function weatherMain() {
+    addEventListeners();
+}
+
+function addEventListeners() {
+    const stationPicker = document.querySelector("#station-btn")
+    stationPicker.addEventListener("click", (event) => {
+        getData();
+        event.preventDefault();
+    })
+}
+
+async function getData() {
+    try {
+        const result = await fetch("https://opendata-download-metobs.smhi.se/api/version/1.0/parameter/1/station-set/all/period/latest-hour/data.json")
+        const data = await result.json()
+        console.log(data.station)
+    } catch(error) {
+    }
+}
