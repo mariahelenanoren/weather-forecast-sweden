d7b586da096e6d6ce008a49ccff1cf9da1881768
mariahelenanoren <marialinder97@hotmail.com>
2020-12-21 13:52:04 +0100

    added JSDOC and fixed typings

--- src/forecast-start.ts
+++ src/forecast-start.ts
@@ -1,4 +1,5 @@
-async function getForecast(lon, lat) {
+/** Gets SMHI forecast API */
+async function getForecast(lon: string, lat: string) {
     try {
         const result = await fetch("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/" + lon + "/lat/" + lat + "/data.json")
         const data = await result.json()
@@ -7,6 +8,7 @@ async function getForecast(lon, lat) {
     }
 }
 
+/** Presents forecast data */
 async function presentForecastData() {
     const data = await getForecast(chosenCity.longitude, chosenCity.latitude);
     
@@ -17,6 +19,7 @@ async function presentForecastData() {
     present9DayForecast(data)
 }
 
+/** Presents city and municipality name */
 function presentCityName() {
     const cityTarget = document.querySelector("#name")
     const municipalityTarget = document.querySelector("#municipality")
@@ -24,6 +27,7 @@ function presentCityName() {
     municipalityTarget.innerHTML = chosenCity.municipality
 }
 
+/** Changes favorite symbol */
 function changeFavoriteSymbol() {
     if (checkIfFavorite(chosenCity.locality, chosenCity.municipality)) {
         document.querySelector("#favorite").innerHTML = "favorite"
